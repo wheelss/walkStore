4bf5dfeafe0b23c6450bf57ef92eb7bd3fd47c8a
wheelss <2323539733@qq.com>
2020-04-28 11:44:27 +0800

    add app

---
+++ sc-provider/sc-xzsd-app/src/main/java/com/xzsd/app/driverHome/entity/Lists.java
@@ -0,0 +1,15 @@
+package com.xzsd.app.driverHome.entity;
+
+import java.util.List;
+
+public class Lists {
+    private List<DriverHomeInfo> list;
+
+    public List<DriverHomeInfo> getList() {
+        return list;
+    }
+
+    public void setList(List<DriverHomeInfo> list) {
+        this.list = list;
+    }
+}
---
+++ sc-provider/sc-xzsd-app/src/main/java/com/xzsd/app/managerInfo/entity/Lists.java
@@ -0,0 +1,15 @@
+package com.xzsd.app.managerInfo.entity;
+
+import java.util.List;
+
+public class Lists {
+    private List<ManagerInfo> list;
+
+    public List<ManagerInfo> getList() {
+        return list;
+    }
+
+    public void setList(List<ManagerInfo> list) {
+        this.list = list;
+    }
+}
